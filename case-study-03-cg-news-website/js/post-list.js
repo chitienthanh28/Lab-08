9ad52d9889593955272ee71b42e5f7371d3e4f72
chitienthanh28 <141891869+chitienthanh28@users.noreply.github.com>
2023-10-23 20:59:40 +0700

    Add files via upload

---
+++ case-study-03-cg-news-website/js/post-list.js
@@ -0,0 +1,6 @@
+let localStTitle = localStorage.getItem('title');
+let localStCategory = localStorage.getItem('category');
+let newsAPI = 'Updating...';
+
+localStorage.setItem('title', '');
+localStorage.setItem('category', '');
