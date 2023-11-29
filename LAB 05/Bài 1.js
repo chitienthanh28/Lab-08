5645a0411122a3dbf6da9fc589f4e1899efa517a
chitienthanh28 <141891869+chitienthanh28@users.noreply.github.com>
2023-11-29 19:20:02 +0700

    Delete LAB 05 directory

--- LAB 05/Bài 1.js
+++
@@ -1,9 +0,0 @@
-function tongGiaTri(...a) {
-  return a.reduce((total, value) => total + value, 0);
-}
-
-const result1 = tongGiaTri(1, 3, 5, 6, 7);
-const result2 = tongGiaTri(10, 15, 20);
-
-console.log(result1);
-console.log(result2);
